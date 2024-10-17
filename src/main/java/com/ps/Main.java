b1da4cc6d48da8140a01b637bf71bdf45468c091
Taylor <taylorsmayberry18@gmail.com>
2024-10-17 09:58:46 -0500

    first commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,26 @@
 package com.ps;
 
+import javax.swing.text.DateFormatter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Main {
+    static Scanner commandScanner = new Scanner(System.in);
+    static Scanner inputScanner = new Scanner(System.in);
+    static ArrayList<Transaction> transaction = new ArrayList<Transaction>();
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
 }
+
---
+++ src/main/java/com/ps/Transaction.java
@@ -0,0 +1,10 @@
+package com.ps;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Transaction {
+ // properties
+
+
+}
